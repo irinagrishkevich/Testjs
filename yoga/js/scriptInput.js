abe41caf6f9c49178b560c71f36df8bc10b4ad9f
Irina <bonivomal1996@gmail.com>
2021-05-06 19:48:32 +0200

    no message

---
+++ yoga/js/scriptInput.js
@@ -0,0 +1,9 @@
+let age = document.getElementById('age')
+
+function showUser(surname, name) {
+  alert(
+    'Пользователь ' + surname + ' ' + name + ', его возраст ' + this.age.value,
+  )
+}
+
+showUser('Smit', 'Jack')
